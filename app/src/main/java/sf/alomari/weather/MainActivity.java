eb763ec9fb468814d89c2c098aaefc47e3bab482
Sohaib Alomari <alomari.sf@gmail.com>
2018-12-17 15:09:04 -0800

    working on the Adapter Class inflating the RecyclerView and next parsing the Json

--- app/src/main/java/sf/alomari/weather/MainActivity.java
+++ app/src/main/java/sf/alomari/weather/MainActivity.java
@@ -24,6 +24,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private RecyclerView recyclerView;
     private ProgressBar mLoadingIndicator;
 
+    private  String Weather_URL ="http://api.openweathermap.org/data/2.5/forecast?q="+String.valueOf(cityName)+"&appid=d7c871940c676a6d8df322f124cc90a0&units=imperial";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,19 +34,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
         mLoadingIndicator = (ProgressBar) findViewById(R.id.pb_loading_indicator);
-        recyclerView=(RecyclerView)findViewById(R.id.recyclerview_forecast);
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerview_forecast);
         recyclerView.setHasFixedSize(true);
-        LinearLayoutManager layoutManager=new LinearLayoutManager(this);
-        recyclerView.addItemDecoration(new DividerItemDecoration(this,DividerItemDecoration.VERTICAL));
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
         recyclerView.setLayoutManager(layoutManager);
-        searchButton=(Button)findViewById(R.id.search_button);
-        searchText=(EditText)MainActivity.this.findViewById(R.id.search_box);
+        searchButton = (Button) findViewById(R.id.search_button);
+        searchText = (EditText) MainActivity.this.findViewById(R.id.search_box);
 
         searchButton.setOnClickListener(this);
-
-
-
-
 
 
     }
@@ -51,27 +50,62 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View view) {
 
+        // Show spinner when starting the search
+        mLoadingIndicator.setVisibility(View.VISIBLE);
+
+        ///get the city name from EditText
+        cityName = searchText.getText().toString();
+
+        //Start a background task using AsynkTask
+        new WeatherQuery().execute();
+
+        //Hide the RecyclerView when starting a new search
+        recyclerView.setVisibility(View.INVISIBLE);
     }
 
 
-    public class WeatherQuery extends AsyncTask<URL,Void ,String>
-    {
+    public class WeatherQuery extends AsyncTask<URL, Void, String> {
+
 
         @Override
         protected String doInBackground(URL... urls) {
-             return null;
-        }
+            String result = null;
 
+            try {
+
+                Log.d("citys", cityName);
+                String postCity = cityName;
+
+                // had to hardCode the URL because when i defined it as a Global variable it would Not take the city name from the EditBox, i looked it up on Stack overflow
+                //people are saying it's some kind of bug in EditText.
+                result = NetworkUtils.run("http://api.openweathermap.org/data/2.5/forecast?q=" + postCity + "&appid=d7c871940c676a6d8df322f124cc90a0&units=imperial");
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return result;
+        }
 
         @Override
         protected void onPostExecute(String s) {
+            String postCity = cityName;
+
+            Log.d("ReturnString", s + " " + postCity + "   " + Weather_URL);
+
+            String result = s;
+
+            mLoadingIndicator.setVisibility(View.INVISIBLE);
+            recyclerView.setVisibility(View.VISIBLE);
+
+            adapter = new WeatherAdapter();
 
 
-
+            recyclerView.setAdapter(adapter);
 
 
         }
-    }
 
+    }
 }
 
--- app/src/main/java/sf/alomari/weather/WeatherAdapter.java
+++ app/src/main/java/sf/alomari/weather/WeatherAdapter.java
@@ -1,17 +1,35 @@
 package sf.alomari.weather;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class WeatherAdapter extends RecyclerView.Adapter<WeatherAdapter.WeatherHolder>{
 
 
+    //All the Arrays I am going to need to parse the Json
+
+    public static String [] Icon_ids;
+    public  static String [] DateArr;
+    public static String [] condition;
+    public static String [] highTemp;
+    public static String [] lowTemp;
+
     @NonNull
     @Override
     public WeatherHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return null;
+        Context context=viewGroup.getContext();
+        int LayoutId=R.layout.adapter;
+        LayoutInflater inflater= LayoutInflater.from(context);
+        View view=inflater.inflate(LayoutId,viewGroup,false);
+        WeatherHolder wHolder=new WeatherHolder( view);
+
+        return wHolder;
     }
 
     @Override
@@ -21,13 +39,27 @@ public class WeatherAdapter extends RecyclerView.Adapter<WeatherAdapter.WeatherH
 
     @Override
     public int getItemCount() {
-        return 0;
+        //chose 7 to display a whole week
+        return 7;
     }
 
     class WeatherHolder extends RecyclerView.ViewHolder{
 
+        TextView date_xml,desc,hi,lo;
+
+        ImageView img;
+
+
          public WeatherHolder(@NonNull View itemView) {
             super(itemView);
+
+
+             img =(ImageView) itemView.findViewById((R.id.weather_icon));
+             date_xml=(TextView)itemView.findViewById(R.id.date_xml);
+             desc=(TextView)itemView.findViewById(R.id.condition_xml);
+             hi=(TextView)itemView.findViewById(R.id.high_xml);
+             lo =(TextView)itemView.findViewById(R.id.low_xml);
+
         }
     }
 
